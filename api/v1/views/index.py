47e0ed8bd74a9bd6059f7410eecd4d144988c9e2
Thato Mongwe <thatomongwe47@gmail.com>
2024-08-05 21:29:13 +0200

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """Index view for API"""
 
 from flask import Blueprint, jsonify
+from models import storage
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
@@ -9,3 +10,13 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 def status():
     """Return the status of the API"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """Retrieve the number of each object by type"""
+    classes = ["Amenity", "City", "Place", "Review", "State", "User"]
+    stats = {}
+    for cls in classes:
+        cls_name = cls.lower() + 's'
+        stats[cls_name] = storage.count(eval(cls))
+    return jsonify(stats)
