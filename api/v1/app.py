50128b734d839a1fc1177ca62f71a86d5fac1014
Thato Mongwe <thatomongwe47@gmail.com>
2024-08-05 21:15:51 +0000

    Add JSON-formatted 404 error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
 app.py
 """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -10,11 +10,16 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Closes the storage"""
+def teardown(exception):
+    """Closes the storage session"""
     storage.close()
 
+@app.errorhandler(404)
+def not_found(error):
+    """Returns a JSON-formatted 404 error response"""
+    return jsonify({"error": "Not found"}), 404
+
 if __name__ == "__main__":
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', 5000)),
+    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(getenv('HBNB_API_PORT', 5000)),
             threaded=True)
