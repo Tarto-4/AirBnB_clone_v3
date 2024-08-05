d84a6d477e439357a98df93fda18135d9a4e14c8
Thato Mongwe <thatomongwe47@gmail.com>
2024-08-05 21:44:18 +0000

    Flask app and register blueprints

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
+"""Initialize Flask app and register blueprints"""
 
-from flask import Flask, jsonify
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
-
+app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def close_storage(self):
+def teardown(exception):
+    """Close storage"""
     storage.close()
 
-
-app.register_blueprint(app_views, url_prefix='/api/v1')
-
 if __name__ == "__main__":
-    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.environ.get('HBNB_API_PORT', 5000))
+    from os import getenv
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
